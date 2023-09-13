dc7ed0fee2c39f1fa4742b2f9c011c6a01f1abb5
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-09-13 15:02:25 -0700

    Multiply

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new = a_dictionary.copy()
+    list_keys = list(new.keys())
+
+    for i in list_keys:
+        new[i] *= 2
+
+    return (new)
