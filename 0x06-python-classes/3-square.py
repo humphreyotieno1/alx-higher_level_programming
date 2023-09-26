34baf305487318d4cd8c46b4a01a9768cf610ffe
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-09-26 13:34:15 -0700

    Updates

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,7 +12,7 @@ class Square:
             size (int): size of new square.
         """
 
-        if type(value) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
