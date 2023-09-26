7798c5e67f6c0859e8783c48e4f1b5ebfd931380
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-09-26 12:57:33 -0700

    square validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Def class Square"""
+
+
+class Square:
+    """Rep class Square"""
+    size: def __init__(self, size=0):
+        """
+        Initialise new square
+        Args:
+            size(int): size of new square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
