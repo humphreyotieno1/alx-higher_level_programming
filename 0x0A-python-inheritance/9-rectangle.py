60693802d1b2074235a4cfb0de8ef0501a4e1669
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-09 14:47:35 -0700

    Full rectangle and square

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
 
         self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
         def area(self):
