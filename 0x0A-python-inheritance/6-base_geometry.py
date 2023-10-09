ca330aec9835f8f147eee104497f3a17f6735d31
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-09 14:05:52 -0700

    Base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry"""
+
+class BaseGeometry:
+    """Define base geometry"""
+
+    def area(self):
+        """Unimplemented"""
+        raise Exception("area() is not implemented")
