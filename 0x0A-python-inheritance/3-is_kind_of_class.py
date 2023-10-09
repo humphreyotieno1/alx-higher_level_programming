037fd1ee03acee870712e759cd9c2a72a70e041b
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-09 13:53:20 -0700

    Same clss or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define class and inherited xlass check function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if object is an instance or inherited instance of class"""
+
+    return (isinstance(obj, a_class))
