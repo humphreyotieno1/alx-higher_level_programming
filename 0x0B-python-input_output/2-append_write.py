f4f551e265d36c572c270bfcfd326ebfbeb64c0d
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-10 06:44:28 -0700

    Append text to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -11,4 +11,4 @@ def append_write(filename="", text=""):
         number of characters added
     """
     with open(filename, "a", encoding="utf-8") as f:
-        return f.append(text)
+        return f.write(text)
