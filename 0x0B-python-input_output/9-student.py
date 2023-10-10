d05f3384c487f94289b04d35655a67c2936edfbd
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-10 08:00:35 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a class called Student"""
+
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """Inialize new Student
+        Args:
+            first_name (str): first name of student
+            last_name (str): last name of student
+            age (int):age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """Return dict rep of Student"""
+            return self.__dict__
