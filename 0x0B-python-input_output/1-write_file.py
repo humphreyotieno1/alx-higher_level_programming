22ea0371aaec5174785b322e1d04d33f3ddabcc2
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-10 06:36:19 -0700

    Write to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,12 @@
 
 
 def write_file(filename="", text=""):
-    """Write string to the text file"""
+    """Write string to the text file
+    Args:
+        filename (str): name of text file
+        text (str): text to add to the file
+    Returns:
+        number of characters written
+    """
     with open(filename, "w" encoding="utf-8") as f:
         return f.write(text)
