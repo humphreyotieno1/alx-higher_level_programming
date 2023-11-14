de55808272e827d8c76deaf119ade6f74d86ec7d
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-11-14 13:46:45 -0800

    List all records

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- list all records of second_table
 
-SELECT scoe, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
