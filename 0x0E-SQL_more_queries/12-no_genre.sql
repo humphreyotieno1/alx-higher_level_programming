faef70e0e72fd55e3e48b909c3b9f68b5744d4b3
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-11-15 14:25:35 -0800

    List shows

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- list all shows in database
+
+SELECT tv_shows.title, tv_show_genres_genre_id
+  FROM tv_shows
+LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+  ORDER BY tv_shows.title, tv_sow_genres.genre_id ASC;
