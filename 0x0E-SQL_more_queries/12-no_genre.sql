ac258328ea0a9c87690289c670af560f49088ce8
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-11-15 14:27:01 -0800

    List shows

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- list all shows in database
 
-SELECT tv_shows.title, tv_show_genres_genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
 LEFT JOIN tv_show_genres
        ON tv_shows.id = tv_show_genres.show_id
