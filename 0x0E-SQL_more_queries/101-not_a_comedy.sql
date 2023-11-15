ad3f07fab2a0aa0306ed5d10a4c4749a743326e0
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-11-15 15:13:58 -0800

    Non comedy

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- list all non Comedy in the database
+
+SELECT tv_shows.title
+  FROM tv_shows
+ WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title
+  FROM tv_shows
+INNER JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_shows.id
+    WHERE tv_genres.name = "Comedy")
+  ORDER BY tv_shows.title ASC;
