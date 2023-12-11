1d3dadaba97ab2aef4d56c9dcd92dfa9320487b2
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-11 13:49:09 -0800

    For conditional

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
 if (isNaN(number)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing number of occurences');
 } else {
-	for (let i = number; i > 0; i -= 1) {
-	  console.log('C is fun');
-	}
+  for (let i = number; i > 0; i -= 1) {
+    console.log('C is fun');
+  }
 }
