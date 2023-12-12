c0def4eb6999ba694381ff5167cdbf46dcc2a2ed
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-12 15:57:10 -0800

    Cocatenation

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,5 +4,5 @@ let fileB = process.argv[3];
 let fileC = process.argv[4];
 const fs = require('fs');
 let textA = fs.readFileSync(fileA, 'utf8');
-let textB = fs.readFileSync(fileA, 'utf8');
+let textB = fs.readFileSync(fileB, 'utf8');
 fs.writeFileSync(fileC, textA + textB);
