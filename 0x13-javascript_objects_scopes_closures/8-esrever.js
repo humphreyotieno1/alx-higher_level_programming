6ff4745af6b9148ffb93437cef50a6d7b3121a4f
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-12 15:30:37 -0800

    Return reverse of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let a = 0;
+  let end = list.length - 1;
+  while (a < end) {
+    let p = list[a];
+    list[a] = list[end];
+    list[end] = p;
+    a++;
+    end--;
+  }
+  return list;
+};
