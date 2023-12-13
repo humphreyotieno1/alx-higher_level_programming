6749239bc65f1d9b8d6127f1ea66d94d72ee5c85
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-13 13:47:43 -0800

    Correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ exports.esrever = function (list) {
   let a = 0;
   let end = list.length - 1;
   while (a < end) {
-    let p = list[a];
+    const p = list[a];
     list[a] = list[end];
     list[end] = p;
     a++;
