9686ff5b0fa96b598267b2d248c1aa43f00aee4e
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-05 13:53:14 -0800

    Sorting and finding the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define function to find peak in an unsorted integer list"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
