3f38c98a918fcccfdd34a7b5b00d2515a3de0749
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-07 14:36:05 -0800

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script to send request to URL and display body"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
