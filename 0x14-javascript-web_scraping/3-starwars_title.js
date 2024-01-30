b35b95a0b85ae981f9bbb13a0ea25d9d68476f75
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-30 23:04:17 +0000

    Starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,24 +1,14 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
 const request = require('request');
+const filmId = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+const myUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+request({ myUrl, json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
   } else if (response.statusCode === 200) {
-    let films = JSON.parse(body).results;
-    let count = 0;
-    for (let i in films) {
-      let chars = films[i].characters;
-      for (let c in chars) {
-	if (chars[c].includes('18')) {
-	  count++;
-	}
-      }
-    }
-    console.log(count);
-  } else {
-    console.log('Erorr Code:' + response.statusCode);
+    console.log(body.title);
   }
 });
